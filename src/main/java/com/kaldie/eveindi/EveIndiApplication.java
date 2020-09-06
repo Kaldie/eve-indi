cddfdee7aa0febbf30d8b68653d28ee3890da998
Ruud Cools <ruud.cools@codecentric.nl>
2020-09-06 17:25:24 +0200

    first try

---
+++ src/main/java/com/kaldie/eveindi/EveIndiApplication.java
@@ -0,0 +1,13 @@
+package com.kaldie.eveindi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveIndiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EveIndiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kaldie/eveindi/EveIndiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaldie.eveindi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EveIndiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
